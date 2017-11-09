65c7a8b5cab49d2102d2bd48277fdc66b4c4a75a
Mindi <slaw.malcz@wp.pl>
2017-11-09 22:23:31 +0100

    Cleaning using in Models

--- WpfApp/Model/NeuralLayer.cs
+++ WpfApp/Model/NeuralLayer.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace WpfApp.Model
 {
